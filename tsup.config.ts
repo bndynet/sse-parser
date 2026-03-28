05a698d004f8e7bbd1a98bfce822dcd4f9682d60
Bendy Zhang <zb@bndy.net>
2026-03-28 10:36:30 +0800

    feat: build iife

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm', 'cjs', 'iife'],
   dts: true,
   sourcemap: true,
   clean: true,
   minify: true,
   target: 'es2020',
   outDir: 'dist',
+  globalName: 'ssep',
 });
